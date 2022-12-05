94f76a05147a98c978dadeb36cad1df22bf4c285
iTrishank <trishank9999@gmail.com>
2022-12-05 22:31:22 +0300

    Updates

---
+++ precache-manifest.2d79da513d504e307125515995d9ea91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ef1ac8df848bfa4d4eedbd0e2c28fe",
+    "url": "/Personal_chatbox/index.html"
+  },
+  {
+    "revision": "b719ec3f4c19bec7d338",
+    "url": "/Personal_chatbox/static/css/main.0afb268b.chunk.css"
+  },
+  {
+    "revision": "96f83cf617c5ed097426",
+    "url": "/Personal_chatbox/static/js/2.bf250779.chunk.js"
+  },
+  {
+    "revision": "b719ec3f4c19bec7d338",
+    "url": "/Personal_chatbox/static/js/main.36b785ff.chunk.js"
+  },
+  {
+    "revision": "eba525aa4ffcbabef82c",
+    "url": "/Personal_chatbox/static/js/runtime-main.c047b531.js"
+  },
+  {
+    "revision": "36cb8b4b9707db874f7e57a8e4495201",
+    "url": "/Personal_chatbox/static/media/chat.36cb8b4b.png"
+  },
+  {
+    "revision": "5483b3de49e66a86329dd2fa54c5bee6",
+    "url": "/Personal_chatbox/static/media/code.5483b3de.jpg"
+  }
+]);
